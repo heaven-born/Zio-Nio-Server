dea2404bee6bd6a8ed695343eaf9784d12c665e0
heaven-born <bogdan.vakulenko@gmail.com>
2020-08-13 17:24:25 +0300

    extracted zio-nio-core server

--- src/main/scala/tserver/StreamsBasedServer.scala
+++ src/main/scala/tserver/StreamsBasedServer.scala
@@ -2,73 +2,38 @@ package tserver
 
 
 import java.nio.ByteBuffer
-
-import cats.implicits.toTraverseOps
 import scodec.bits.ByteVector
+import tserver.common.ZioNioTcpServer
 import zio.console.Console
 import zio.{RIO, _}
-import zio.nio.channels._
-import zio.nio.core.SocketAddress
-import zio.stream._
 import zio.interop.catz.{console => _, _}
 
 import scala.language.implicitConversions
 
 object StreamsBasedServer extends App {
 
-  def run(args: List[String]): URIO[Console, ExitCode] =
-    server(8080)
-      .use(handleConnections(_, processor,pDecoder,pEncoder))
+  type Env = Console
+
+  def run(args: List[String]): URIO[Env, ExitCode] =
+    new ZioNioTcpServer[Env,Protocol]("localhost",8080)
+      .run(processor,encoder,decoder)
       .exitCode
 
-
-
-  def server(port: Int): Managed[Exception, AsynchronousServerSocketChannel] =
-    for {
-      server        <- AsynchronousServerSocketChannel()
-      socketAddress <- SocketAddress.inetSocketAddress(port).toManaged_
-      _             <- server.bind(socketAddress).toManaged_
-    } yield server
-
-  def handleConnections[R <: Console](
-                                     server: AsynchronousServerSocketChannel,
-                                     processor: Protocol => RIO[R, List[Protocol]],
-                                     protocolDecoder: Array[Byte] => RIO[R,Protocol],
-                                     protocolEncoder: Protocol => RIO[R,Array[Byte]]): RIO[R, Unit] =
-    ZStream
-      .repeat(server.accept)
-      .flatMap(conn => ZStream.managed(conn.ensuring(console.putStrLn("Connection closed"))))
-      .mapM { channel =>
-            val r = for {
-              _    <- console.putStrLn("Received connection")
-              data <- channel.read(256)
-              protocolObject <- protocolDecoder(data.toArray)
-              respObjects    <- processor(protocolObject)
-              respEncoded    <- respObjects.map(pEncoder).sequence
-              writtenBytes <- respEncoded.map(arr => channel.write(Chunk.fromArray(arr))).sequence
-              _    <- console.putStrLn("Bytes written: "+writtenBytes)
-            } yield ()
-            r.catchAllDefect(e=>URIO(e.printStackTrace()))
-             .catchAll(e=>URIO(e.printStackTrace()))
-      }.runDrain
-
-  def processor[R <: Console](proto: Protocol):RIO[R, List[Protocol]] = {
-      console.putStrLn(s"Read data: ${proto.toString}") *>
-      console.putStrLn("Done") *>
+  def processor(proto: Protocol):RIO[Env, List[Protocol]] = {
       RIO(List[Protocol](ResPQ(1,3,4,5,ByteVector.fromInt(5))))
   }
 
-  def pEncoder[R <: Console](arr: Protocol):RIO[R, Array[Byte]] = arr match {
+  def encoder(arr: Protocol):RIO[Env, Array[Byte]] = arr match {
     case res: ResPQ =>
       val vec = ResPQCodec.encode(res).require
-     console.putStrLn(s"Write data: ${vec}") *>
+     console.putStrLn(s"Response Hex: ${vec.toByteVector}") *>
       ZIO(vec.toByteArray)
     case e => ZIO.fail(new Exception("Encoding not supported for:"+e))
   }
 
 
-  def pDecoder[R](arr: Array[Byte]):RIO[R, Protocol]  = {
-    println("Hex: "+ByteVector.apply(arr))
+  def decoder(arr: Array[Byte]):RIO[Env, Protocol]  = {
+    println("Request Hex: "+ByteVector.apply(arr))
     val buffer = arr.slice(20,24)
     val constructor = ByteBuffer.wrap(buffer.reverse).getInt
     println("TL constructor (decimal): "+ constructor)
---
+++ src/main/scala/tserver/common/ZioNioTcpServer.scala
@@ -0,0 +1,52 @@
+package tserver.common
+
+import cats.implicits.toTraverseOps
+import tserver.Protocol
+import zio.{Chunk, Managed, RIO, URIO, console}
+import zio.console.Console
+import zio.nio.channels.AsynchronousServerSocketChannel
+import zio.nio.core.SocketAddress
+import zio.stream.ZStream
+import zio.interop.catz.{console => _, _}
+
+class ZioNioTcpServer[R <: Console,T](host: String ,port: Int) {
+
+  def run(processor: Protocol => RIO[R, List[Protocol]],
+          encoder: Protocol => RIO[R,Array[Byte]],
+          decoder: Array[Byte] => RIO[R,Protocol]): RIO[R, Unit] =
+    server(host,port)
+      .use(handleConnections(_, processor,decoder,encoder))
+
+
+
+  private def server(host: String ,port: Int): Managed[Exception, AsynchronousServerSocketChannel] =
+    for {
+      server        <- AsynchronousServerSocketChannel()
+      socketAddress <- SocketAddress.inetSocketAddress(host,port).toManaged_
+      _             <- server.bind(socketAddress).toManaged_
+    } yield server
+
+  private def handleConnections( server: AsynchronousServerSocketChannel,
+                                 processor: Protocol => RIO[R, List[Protocol]],
+                                 protocolDecoder: Array[Byte] => RIO[R,Protocol],
+                                 protocolEncoder: Protocol => RIO[R,Array[Byte]]): RIO[R, Unit] =
+    ZStream
+      .repeat(server.accept)
+      .flatMap(conn => ZStream.managed(conn.ensuring(console.putStrLn("Connection closed"))))
+      .mapM { channel =>
+        val r = for {
+          _    <- console.putStrLn("Received connection")
+          requestData <- channel.read(256)
+          requestObject <- protocolDecoder(requestData.toArray)
+          _    <- console.putStrLn("Data received: "+requestObject)
+          respObjects    <- processor(requestObject)
+          _    <- console.putStrLn("Response to be sent: "+respObjects)
+          respData    <- respObjects.map(protocolEncoder).sequence
+          writtenBytes <- respData.map(arr => channel.write(Chunk.fromArray(arr))).sequence
+          _    <- console.putStrLn("Bytes sent: "+writtenBytes)
+        } yield ()
+        r.catchAllDefect(e=>URIO(e.printStackTrace()))
+          .catchAll(e=>URIO(e.printStackTrace()))
+      }.runDrain
+
+}
